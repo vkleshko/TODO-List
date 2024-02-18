972de23ced574f902120736598c29bd32aac6f2b
Vlad Kleshko <vkoesko@gmail.com>
2024-02-18 23:58:05 +0200

    Created models

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(auto_now_add=False)
+    done = models.BooleanField(default=False)
+    tag = models.ForeignKey(
+        Tag,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+
+    def __str__(self):
+        return f"[{self.content}]; {self.tag}"
+
+    class Meta:
+        ordering = ["created"]
