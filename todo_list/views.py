6bc26e146c9d2a02e761435ad031f522c5115f3d
Vlad Kleshko <vkoesko@gmail.com>
2024-02-19 21:57:29 +0200

    Created TaskUpdateView view

--- todo_list/views.py
+++ todo_list/views.py
@@ -15,3 +15,10 @@ class TaskCreateView(generic.CreateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("todo_list:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = "todo_list/task_form.html"
+
