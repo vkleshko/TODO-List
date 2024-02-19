ab62435003a62ec84454f89471310c593923799a
Vlad Kleshko <vkoesko@gmail.com>
2024-02-19 21:26:31 +0200

    Created 'task/create/' url

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from todo_list.views import TaskListView
+from todo_list.views import (
+    TaskListView,
+    TaskCreateView,
+)
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="index")
+    path("", TaskListView.as_view(), name="index"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create")
 ]
 
 app_name = "todo_list"
