f2596203804cb93650feac952a637525c4f56cd2
Vlad Kleshko <vkoesko@gmail.com>
2024-02-19 21:57:45 +0200

    Created 'task/update/<int:pk>/' url

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,11 +3,14 @@ from django.urls import path
 from todo_list.views import (
     TaskListView,
     TaskCreateView,
+    TaskUpdateView,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
-    path("task/create/", TaskCreateView.as_view(), name="task-create")
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("task/update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+
 ]
 
 app_name = "todo_list"
