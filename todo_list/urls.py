1e3cfe3abe419a45a16a196c2ccdc5a5d5b8fae7
Vlad Kleshko <vkoesko@gmail.com>
2024-02-19 22:15:00 +0200

    Created 'task/delete/<int:pk>/' url

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -4,12 +4,14 @@ from todo_list.views import (
     TaskListView,
     TaskCreateView,
     TaskUpdateView,
+    TaskDeleteView,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
     path("task/create/", TaskCreateView.as_view(), name="task-create"),
     path("task/update/<int:pk>/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/delete/<int:pk>/", TaskDeleteView.as_view(), name="task-delete"),
 
 ]
 
