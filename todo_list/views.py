e2fa77d15d868c8204ed9a7b7e336bc868ecc0e0
Vlad Kleshko <vkoesko@gmail.com>
2024-02-19 21:26:09 +0200

    Created TaskCreateView view

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,5 +1,7 @@
 from django.views import generic
+from django.urls import reverse_lazy
 
+from todo_list.forms import TaskForm
 from todo_list.models import Task
 
 
@@ -7,3 +9,9 @@ class TaskListView(generic.ListView):
     queryset = Task.objects.all()
     context_object_name = "task_list"
     template_name = "todo_list/task_list.html"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo_list:index")
