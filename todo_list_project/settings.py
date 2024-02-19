4e8ee1c34651405aa59dfe624c8d6e1ae1ef0b55
Vlad Kleshko <vkoesko@gmail.com>
2024-02-19 23:50:26 +0200

    Added usage of environment variables in project configuration

--- todo_list_project/settings.py
+++ todo_list_project/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)xmuu-22-79r#-*l-a%tucpyq8_q(qo+xs$ns(8xv&-c&1pzd(" #TODO: move to .env
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
